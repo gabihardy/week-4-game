8cdf7bb91bf0817728b23942b6ccf95ed533d6fd
Gabrielle Hardy <gabrhardy@gmail.com>
2016-08-25 19:48:47 -0500

    added images/bootstrap classes/working on progress bar

---
+++ js/game.js
@@ -0,0 +1,21 @@
+/* var needed:
+	cat happiness
+	cat clicked
+
+
+	functions that:
+	-make actions add to progress bar
+		(in diff amounts depending on action)
+	-actions may randomly be unsuccessful
+		& deplete cat happiness
+	-give cat counter attacks that may be
+		neutral, positive, or negative to score.
+	-make a modal pop up when click on cat
+	-play meow sound when cat clicked
+	-play cutesy 8-bit music in background
+	-when catHapp reaches 100%, you win
+	-if catHapp reaches 0%, you lose
+
+	progress bar for cat happiness = catHapp
+*/
+	
